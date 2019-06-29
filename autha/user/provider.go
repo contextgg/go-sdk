62c61c70ac18e5f30a376c5f1a96cdad2101c607
Chris Kolenko <chris@webcanvas.com.au>
2019-06-29 10:45:56 +1000

    Add func

--- autha/user/provider.go
+++ autha/user/provider.go
@@ -5,6 +5,10 @@ import "github.com/contextgg/go-sdk/autha"
 type provider struct {
 }
 
+func (p *provider) Login(connection string, id *autha.Identity, token autha.Token) (*autha.User, error) {
+	return nil, nil
+}
+
 // NewProvider creates a new user provider
 func NewProvider(name string) autha.UserProvider {
 	return &provider{}
